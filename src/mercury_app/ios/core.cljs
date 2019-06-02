4e26c5e4149e05c03723d94c18755e86c85aa55e
Oliver George <oliver@condense.com.au>
2019-06-02 22:59:02 +1000

    Use interop.react-native

--- src/mercury_app/ios/core.cljs
+++ src/mercury_app/ios/core.cljs
@@ -2,32 +2,23 @@
   (:require [reagent.core :as r :refer [atom]]
             [re-frame.core :refer [subscribe dispatch dispatch-sync]]
             [mercury-app.events]
-            [mercury-app.subs]))
+            [mercury-app.subs]
+            [interop.react-native :as rn]))
 
-(def ReactNative (js/require "react-native"))
-
-(def app-registry (.-AppRegistry ReactNative))
-(def text (r/adapt-react-class (.-Text ReactNative)))
-(def view (r/adapt-react-class (.-View ReactNative)))
-(def image (r/adapt-react-class (.-Image ReactNative)))
-(def touchable-highlight (r/adapt-react-class (.-TouchableHighlight ReactNative)))
 
 (def logo-img (js/require "./images/cljs.png"))
-
-(defn alert [title]
-      (.alert (.-Alert ReactNative) title))
 
 (defn app-root []
   (let [greeting (subscribe [:get-greeting])]
     (fn []
-      [view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
-       [text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
-       [image {:source logo-img
+      [rn/view {:style {:flex-direction "column" :margin 40 :align-items "center"}}
+       [rn/text {:style {:font-size 30 :font-weight "100" :margin-bottom 20 :text-align "center"}} @greeting]
+       [rn/image {:source logo-img
                :style  {:width 80 :height 80 :margin-bottom 30}}]
-       [touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
-                             :on-press #(alert "HELLO!")}
-        [text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
+       [rn/touchable-highlight {:style {:background-color "#999" :padding 10 :border-radius 5}
+                             :on-press #(rn/alert "HELLO!")}
+        [rn/text {:style {:color "white" :text-align "center" :font-weight "bold"}} "press me"]]])))
 
 (defn init []
       (dispatch-sync [:initialize-db])
-      (.registerComponent app-registry "MercuryApp" #(r/reactify-component app-root)))
+      (.registerComponent rn/AppRegistry "MercuryApp" #(r/reactify-component app-root)))
